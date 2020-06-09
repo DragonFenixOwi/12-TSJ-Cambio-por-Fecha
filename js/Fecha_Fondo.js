4b247cce258c25415f0206d337e048f331ee68ad
DragonFenixOwi <mecaowi@gmail.com>
2020-06-09 19:25:46 +0200

    Mejorando las funciones

--- js/Fecha_Fondo.js
+++ js/Fecha_Fondo.js
@@ -5,9 +5,12 @@
 
 */
 
+// Variables de extraer fecha del reloj de la compu
+var Dia_de_la_semana = new Date();
+var resultado_final = Dia_de_la_semana.getDay();
 
 /*
-    Acceder a botones
+    Acceder a botones 
 */
 var lunes = document.getElementById("Lunes");
 var martes = document.getElementById("Martes");
@@ -41,6 +44,73 @@ domingo.style.backgroundColor = "#01698e";
 
 
 
+
+/*
+    --------------------
+        FUNCIONES
+    -------------------
+
+*/
+
+
+
+if (resultado_final ==  1) 
+    {
+        document.write(`Día Lunes - Negro - comienza el estres jaja`);
+        document.body.style.backgroundColor = "black";
+    }
+
+if (resultado_final ==  2) 
+    {
+        document.write(`Día Martes - Verde - paz con la naturaleza`);
+        document.body.style.backgroundColor = "green";
+    }
+
+if (resultado_final ==  3) 
+    {
+        document.write(`Día Miercoles - Lila -  color del Rey`);
+        document.body.style.backgroundColor = "purple";
+    }
+
+if (resultado_final ==  4) 
+    {
+        document.write(`Día Jueves - Naranja - Color del cambio`);
+        document.body.style.backgroundColor = "orange";
+    }
+
+if (resultado_final ==  5) 
+    {
+        document.write(`Día Viernes - Azul - Tranquilidad`);
+        document.body.style.backgroundColor = "blue";
+    }
+
+if (resultado_final ==  6) 
+    {
+        document.write(`Día Sabado - Amarillo -  Color de la Felicidad`);
+        document.body.style.backgroundColor = "yellow";
+    }
+    
+if (resultado_final ==  7) 
+    {
+        document.write(`Día Domingo - Blanco - Cerebro en blanco`);
+        document.body.style.backgroundColor = "white";
+    }
+    
+
+
+
+    
+
+
+    
+
+
+    
+
+
+
+
+
 /*
     ----------------
         EVENTOS
@@ -49,28 +119,40 @@ domingo.style.backgroundColor = "#01698e";
 
 
 
-lunes.addEventListener("click", function () 
-    {
-        document.body.style.backgroundColor = "green";
-    });
 
-martes.addEventListener("click", function () 
+/*
+    ------------------------------------
+        ALTERNATIVA PRESIONANDO BOTONES
+    -------------------------------------
+*/
+
+
+/*
+    Vizualizar colores elegidos arriba, para cada fecha
+*/
+
+lunes.addEventListener("click", function () 
     {
         document.body.style.backgroundColor = "black";
     });
 
+martes.addEventListener("click", function () 
+    {
+        document.body.style.backgroundColor = "green";
+    });
+
 miercoles.addEventListener("click", function () 
     {
-        document.body.style.backgroundColor = "blue";
+        document.body.style.backgroundColor = "purple";
     });
 Jueves.addEventListener("click", function () 
     {
-        document.body.style.backgroundColor = "purple";
+        document.body.style.backgroundColor = "orange";
     });
 
 viernes.addEventListener("click", function () 
     {
-        document.body.style.backgroundColor = "orange";
+        document.body.style.backgroundColor = "blue";
     });
 
 sabado.addEventListener("click", function () 
